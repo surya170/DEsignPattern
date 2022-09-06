e81d6c4d4517896148b320278aba9de9df609217
Surya170 <suryanarayana13271@gmail.com>
2022-09-06 22:05:30 +0530

    Eager Instanation

--- DPProj01-SingletonClass/src/com/surya/dp/Printer.java
+++ DPProj01-SingletonClass/src/com/surya/dp/Printer.java
@@ -1,22 +1,28 @@
 package com.surya.dp;
 
 public class Printer {
-
-	private static Printer INSTANCE;
-
+  
+	//private static Printer INSTANCE;
+   private static Printer INSTANCE = new Printer();   // eager Instance()
+   
 	// private constructor
 	private Printer() {
 		System.out.println("Printer:: 0-param Constructor ");
 	}
 
-	// static factory method
+	/*  // static factory method
 	public static Printer getInstance() {
 		// singleton logic
 		if (INSTANCE == null)
 			INSTANCE = new Printer();
 		return INSTANCE;
 	}// method
-
+  */
+	
+	public static Printer getInstance() {
+		 return INSTANCE;
+	}
+	
 	// Business method
 	public void print(String msg) {
 		System.out.println(msg);
