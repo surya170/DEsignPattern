151f0e664b76c1316b02e2d9738fe7a1bdd187fe
Surya170 <suryanarayana13271@gmail.com>
2022-09-18 09:54:43 +0530

    Double Null Check Problem

--- DPProj01-SingletonClass/src/com/surya/dp/Printer.java
+++ DPProj01-SingletonClass/src/com/surya/dp/Printer.java
@@ -2,30 +2,32 @@
 package com.surya.dp;
 
 public class Printer {
-  
+
 	private static Printer INSTANCE;
-    // private static Printer INSTANCE = new Printer();   // eager Instance()
-   
+	// private static Printer INSTANCE = new Printer(); // eager Instance()
+
 	// private constructor
 	private Printer() {
 		System.out.println("Printer:: 0-param Constructor ");
 	}
 
-  // synchronized static factory method
-	public synchronized static Printer getInstance() {
-		// singleton logic
-		if (INSTANCE == null)
-			INSTANCE = new Printer();
-		return INSTANCE;
-		
-	}// method
-  
-	/*
+	// synchronized static factory method
 	public static Printer getInstance() {
-		 return INSTANCE;
-	} 
-	*/
-	
+		// singleton logic
+		if (INSTANCE == null) { // 1st NULL Check
+			synchronized (Printer.class) {
+				if (INSTANCE == null) // 2nd NULL Check
+					INSTANCE = new Printer();
+			}
+		}
+		return INSTANCE;
+
+	} // method
+
+	/*
+	 * public static Printer getInstance() { return INSTANCE; }
+	 */
+
 	// Business method
 	public void print(String msg) {
 		System.out.println(msg);
