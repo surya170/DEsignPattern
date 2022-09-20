dfb2b87d1b6ba9d721f4905eb63dfe4dbdc3dce1
Surya170 <suryanarayana13271@gmail.com>
2022-09-20 11:22:15 +0530

    SubClass of Object Class having implementation of Cloneable Class

---
+++ DPProj01-SingletonClass/src/com/surya/commons/CommonsUtil.java
@@ -0,0 +1,12 @@
+package com.surya.commons;
+
+public class CommonsUtil implements Cloneable {
+	
+	@Override
+	public Object clone() throws CloneNotSupportedException {
+		return super.clone();  // use clone() of java.lang.Object class for cloning process
+	}
+	
+	
+
+}
