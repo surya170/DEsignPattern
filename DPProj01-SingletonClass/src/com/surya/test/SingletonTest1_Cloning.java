fcb7807227813e539253e9fe12f4b494af8ea80c
Surya170 <suryanarayana13271@gmail.com>
2022-09-20 11:23:02 +0530

    SingletonTestClass with Cloning

---
+++ DPProj01-SingletonClass/src/com/surya/test/SingletonTest1_Cloning.java
@@ -0,0 +1,23 @@
+package com.surya.test;
+
+import com.surya.dp.Printer;
+
+public class SingletonTest1_Cloning {
+	
+	public static void main(String[] args) {
+       // Get Singleton java class object 
+		Printer p1 = Printer.getInstance();
+		// do cloning on object 
+		try {
+			Printer p2 = (Printer) p1.clone();
+			System.out.println("Cloned object is created");
+			System.out.println(p1.hashCode()+" "+p2.hashCode());
+			System.out.println("p1===p2?"+(p1==p2));
+		} catch (CloneNotSupportedException cnse) {
+			 cnse.printStackTrace();
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
