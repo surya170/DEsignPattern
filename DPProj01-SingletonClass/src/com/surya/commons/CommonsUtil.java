87e9e2f0c7153da5a43db58cadd1043635f752ea
Surya170 <suryanarayana13271@gmail.com>
2022-09-20 11:24:21 +0530

    CommonsUtil Class with Cloneable  implementation

--- DPProj01-SingletonClass/src/com/surya/commons/CommonsUtil.java
+++ DPProj01-SingletonClass/src/com/surya/commons/CommonsUtil.java
@@ -4,8 +4,9 @@ public class CommonsUtil implements Cloneable {
 	
 	@Override
 	public Object clone() throws CloneNotSupportedException {
-		return super.clone();  // use clone() of java.lang.Object class for cloning process
+		return super.clone();  // Use clone() of java.lang.Object class for cloning process
 	}
+	
 	
 	
 
