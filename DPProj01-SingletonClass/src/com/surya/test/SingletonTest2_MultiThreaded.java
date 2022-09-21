b7b8ed1535f9e4cd3380407ec975fe18725b8af5
Surya170 <suryanarayana13271@gmail.com>
2022-09-21 17:25:27 +0530

    Singleton Java Client class To Resolve MulthiThread Issue In Singleton Class.

--- DPProj01-SingletonClass/src/com/surya/test/SingletonTest2_MultiThreaded.java
+++ DPProj01-SingletonClass/src/com/surya/test/SingletonTest2_MultiThreaded.java
@@ -7,7 +7,7 @@ public class SingletonTest2_MultiThreaded  {
 
 	public static void main(String[] args) throws Exception {
 		
-	  // Creates threads having Servlet Class object as data
+	  // creates threads having Servlet Class object as data
 		TicketBookingServlet servlet = new TicketBookingServlet();
 		Thread t1 = new Thread(servlet);
 	    Thread t2 = new Thread(servlet);
