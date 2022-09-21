91e1fb636df1f27bca946fb46162f873b6dd3882
Surya170 <suryanarayana13271@gmail.com>
2022-09-21 17:24:04 +0530

    TicketBookingServlet Class To Feel MulthiThread issue for singleton in Servelt Angle we developed in Java Fundamentals

--- DPProj01-SingletonClass/src/com/surya/test/TicketBookingServlet.java
+++ DPProj01-SingletonClass/src/com/surya/test/TicketBookingServlet.java
@@ -3,7 +3,8 @@ package com.surya.test;
 import com.surya.dp.Printer1;
 
 public class TicketBookingServlet implements Runnable {
-
+  
+	// run method 
 	@Override
 	public void run() {
         Printer1 p1 = Printer1.getInstance();
