07a55757ba3d9b23b3fb20b3b4b01ce74772c225
Surya170 <suryanarayana13271@gmail.com>
2022-09-21 18:28:28 +0530

    TicketBookingServlet

--- DPProj01-SingletonClass/src/com/surya/test3/TicketBookingServlet.java
+++ DPProj01-SingletonClass/src/com/surya/test3/TicketBookingServlet.java
@@ -1,13 +1,13 @@
 package com.surya.test3;
 
-import com.surya.dp.Printer2;
+import com.surya.dp.Printer3;
 
 public class TicketBookingServlet implements Runnable {
   
 	// run method 
 	@Override
 	public void run() {
-        Printer2 p1 = Printer2.getInstance();
+        Printer3 p1 = Printer3.getInstance();
        System.out.println(p1.hashCode());        
 	}
 	
