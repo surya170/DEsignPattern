1e443b3b8808bcac488f5fa896e8fc8dcf512117
Surya170 <suryanarayana13271@gmail.com>
2022-09-21 18:19:41 +0530

    SingletonTest2 through Cloning

--- DPProj01-SingletonClass/src/com/surya/test2/SingletonTest2_Cloning.java
+++ DPProj01-SingletonClass/src/com/surya/test2/SingletonTest2_Cloning.java
@@ -1,15 +1,15 @@
 package com.surya.test2;
 
-import com.surya.dp.Printer1;
+import com.surya.dp.Printer2;
 
-public class SingletonTest1_Cloning {
+public class SingletonTest2_Cloning {
 	
 	public static void main(String[] args) {
        // Get Singleton java class object
-		Printer1 p1 = Printer1.getInstance();
+		Printer2 p1 = Printer2.getInstance();
 		// do cloning on object 
 		try {
-			Printer1 p2 = (Printer1) p1.clone();
+			Printer2 p2 = (Printer2) p1.clone();
 			System.out.println("Cloned object is created");
 			System.out.println(p1.hashCode()+" "+p2.hashCode());
 			System.out.println("p1===p2?"+(p1==p2)); 
