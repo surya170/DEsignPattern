1edee6301e47a370af66dea024cd57a4d92dda47
Surya170 <suryanarayana13271@gmail.com>
2022-09-21 18:44:31 +0530

    SingletonTest03

--- DPProj01-SingletonClass/src/com/surya/test3/SingletonTest03_ReflectionApi.java
+++ DPProj01-SingletonClass/src/com/surya/test3/SingletonTest03_ReflectionApi.java
@@ -3,12 +3,13 @@ package com.surya.test3;
 import java.lang.reflect.Constructor;
 
 import com.surya.dp.Printer2;
+import com.surya.dp.Printer3;
 
 public class SingletonTest03_ReflectionApi {
 
 	public static void main(String[] args) {
         // get single object of singleton java class using static factory method
-		Printer2 p1 = Printer2.getInstance();
+		Printer3 p1 = Printer3.getInstance();
 		try {
 			 // get java.lang.class object represening printer class
 			Class c = p1.getClass(); // getClass() is public method  of java.lang.Object class
@@ -16,8 +17,8 @@ public class SingletonTest03_ReflectionApi {
 			Constructor[] cons = c.getDeclaredConstructors();
 			cons[0].setAccessible(true);  // give accessed to private constructor.
 			// create object using  the accessed private Constructor 
-			Printer2 p2 = (Printer2) cons[0].newInstance();
-			Printer2 p3 = (Printer2) cons[0].newInstance();
+			Printer3 p2 = (Printer3) cons[0].newInstance();
+			Printer3 p3 = (Printer3) cons[0].newInstance();
 			System.out.println("Objects Are Created by Using Reflection API");
 			System.out.println(p1.hashCode()+" "+p2.hashCode()+" "+p3.hashCode());
 			
