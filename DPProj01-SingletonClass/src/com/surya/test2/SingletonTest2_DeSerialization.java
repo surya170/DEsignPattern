a751aa1a3b0a832f1b75729a42640db358050469
Surya170 <suryanarayana13271@gmail.com>
2022-09-21 18:11:58 +0530

    singletonTest2 Deserializzation

--- DPProj01-SingletonClass/src/com/surya/test2/SingletonTest2_DeSerialization.java
+++ DPProj01-SingletonClass/src/com/surya/test2/SingletonTest2_DeSerialization.java
@@ -5,16 +5,17 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 
 import com.surya.dp.Printer1;
+import com.surya.dp.Printer2;
 
-public class SingletonTest1_DeSerialization {
+public class SingletonTest2_DeSerialization {
 
 	public static void main(String[] args) {
-		Printer1 p1 = Printer1.getInstance();
+		Printer2 p1 = Printer2.getInstance();
 		System.out.println(p1.hashCode());
            try {
         	   // ReCreate Object having data collect from the file using DeSeriliazation ProceSS 
         	   ObjectInputStream ois = new ObjectInputStream(new  FileInputStream("Printer_data.ser"));
-        	   Printer1 p2 = (Printer1) ois.readObject();
+        	   Printer2 p2 = (Printer2) ois.readObject();
         	   p2.print("Hello");
         	   System.out.println("p2 object hashcode ::"+p2.hashCode());
         	   ois.close(); 
@@ -30,7 +31,7 @@ public class SingletonTest1_DeSerialization {
            try {
         	   // ReCreate Object having data collect from the file using DeSeriliazation Process 
         	   ObjectInputStream ois = new ObjectInputStream(new  FileInputStream("Printer_data.ser"));
-        	   Printer1 p3 = (Printer1) ois.readObject();
+        	   Printer2 p3 = (Printer2) ois.readObject();
         	   p3.print("Hello");
         	   System.out.println("p3  object hashcode ::"+p3.hashCode());
         	   ois.close(); 
