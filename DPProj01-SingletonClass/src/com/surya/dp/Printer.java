8c03d5057c620bba5face1fe12d535566cd754fa
Surya170 <suryanarayana13271@gmail.com>
2022-09-21 14:06:03 +0530

    Modified

--- DPProj01-SingletonClass/src/com/surya/dp/Printer.java
+++ DPProj01-SingletonClass/src/com/surya/dp/Printer.java
@@ -15,7 +15,7 @@ public class Printer extends CommonsUtil {
 		  System.out.println("Printer :: 0-Param Constructor");
 	}
 
-	// synchronized static factory method
+	// Synchronized static factory method
 	/*
 	public static Printer getInstance() {
 		// singleton logic
